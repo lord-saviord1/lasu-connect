70b1dba855725ede6143aafe1aa7842463162092
Daniel Savior Ozoemena <ozoemenad4@gmail.com>
2026-08-27 17:55:07 +0100

    Change initial round robin index from 0 to 1

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -19,7 +19,7 @@ function buildAccounts() {
 }
 
 const accounts = buildAccounts();
-let _roundRobinIndex = 0;
+let _roundRobinIndex = 1;
 
 function getNextAccount() {
   const account = accounts[_roundRobinIndex % accounts.length];
